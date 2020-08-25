99493a9aaf6d6de1dc3d04eeabc4bbd5416de89d
ValkoBob <valko_bob@ukr.net>
2020-08-25 14:32:19 +0300

    Make last fixes before sending.

--- script.js
+++ script.js
@@ -20,17 +20,26 @@ const menuFunction = () => {
     const menuTitle = document.getElementById('menu-title');
     const navArrow = document.getElementById('nav-arrow');
     const arrowIcon = document.getElementById('arrow-icon');
+    const wrapper = document.getElementById('wrapper');
     for(let i = 0; i < elements.length; i++){
         const element = elements.item(i);
+        const menuItems = document.getElementsByClassName('menu-item');
         if (element.classList.contains('hide')) {
             element.classList.remove('hide');
             navbar.style.width = '200px';
-            menuTitle.style.marginLeft = '54px 31px 25px 30px';
+            menuTitle.style.margin = '54px 31px 25px 30px';
             navArrow.style.left = '145px'
             logo.style.width = '100px'
             logo.style.height = '60px'
-            navHeader.style.margin = '18px 50px';
+            navHeader.style.padding = '18px 50px';
             arrowIcon.style.transform = 'rotate(0)';
+            wrapper.style.marginLeft = '200px';
+            for(let i = 0; i < menuItems.length; i++) {
+                menuItems[i].style.width = '220px';
+                menuItems[i].style.padding = '10px 30px';
+                menuItems[i].style.margin = '0';
+                menuItems[i].style.justifyContent = 'flex-start';
+            }
         } else {
             element.classList.add('hide');
             navbar.style.width = '84px';
@@ -38,8 +47,15 @@ const menuFunction = () => {
             navArrow.style.left = '42px';
             logo.style.width = '66px';
             logo.style.height = '40px';
-            navHeader.style.margin = '30px 9px 18px';
+            navHeader.style.padding = '30px 9px 26px';
             arrowIcon.style.transform = 'rotate(180deg)';
+            wrapper.style.marginLeft = '84px';
+            for(let i = 0; i < menuItems.length; i++) {
+                menuItems[i].style.width = '64px';
+                menuItems[i].style.padding = '0';
+                menuItems[i].style.margin = '0 10px';
+                menuItems[i].style.justifyContent = 'center';
+            }
         }
     }
 };
