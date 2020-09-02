bfd024624b520c0ff9e58b992b1386fdf4f63fcd
ValkoBob <valko_bob@ukr.net>
2020-09-02 09:27:03 +0300

    Fixes with menu and menu-arrow

--- script.js
+++ script.js
@@ -131,8 +131,6 @@ const subMenuWrapper = document.getElementById('submenu-wrapper');
 const resourcesButtonDesktop = document.getElementById('resources-desktop');
 
 resourcesButtonDesktop.addEventListener('click', () => {
-    const node = document.createElement('div');
-    node.classList.add('menu-item-arrow');
     const currentPosition = resourcesButtonDesktop.getBoundingClientRect().top;
 
     subMenuList.style.top = `${(window.innerHeight - currentPosition) > 220
@@ -141,47 +139,13 @@ resourcesButtonDesktop.addEventListener('click', () => {
     if (subMenu.style.marginLeft === '-300px' && !menuOverlayed) {
         subMenu.style.marginLeft = '200px';
         subMenuWrapper.classList.remove('hide');
-        if (!menuOverlayed) {
-            resourcesButtonDesktop.appendChild(node);
-        }
         navArrow.classList.add('hide');
     } else {
         subMenu.style.marginLeft = '-300px';
         subMenuWrapper.classList.add('hide');
-        const elements = document.getElementsByClassName("menu-item-arrow");
-        if (!menuOverlayed) {
-            while (elements.length > 0) elements[0].remove();
-        }
         navArrow.classList.remove('hide');
     }
 });
-
-/*document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('click', event => {
-        const node = document.createElement('div');
-        node.classList.add('menu-item-arrow');
-        const currentPosition = item.getBoundingClientRect().top;
-        subMenuList.style.top = `${(window.innerHeight - currentPosition) > 220
-            ? currentPosition : (currentPosition - (220 - (window.innerHeight - currentPosition)))}px`;
-
-        if (subMenu.style.marginLeft === '-300px' && !menuOverlayed) {
-            subMenu.style.marginLeft = '200px';
-            subMenuWrapper.classList.remove('hide');
-            if (!menuOverlayed) {
-                item.appendChild(node);
-            }
-            navArrow.classList.add('hide');
-        } else {
-            subMenu.style.marginLeft = '-300px';
-            subMenuWrapper.classList.add('hide');
-            const elements = document.getElementsByClassName("menu-item-arrow");
-            if (!menuOverlayed) {
-                while (elements.length > 0) elements[0].remove();
-            }
-            navArrow.classList.remove('hide');
-        }
-    })
-})*/
 
 const resourcesButtonTablet = document.getElementById('resources-tablet');
 const popOverSubMenu = document.getElementById('popover-submenu');
@@ -190,33 +154,25 @@ const popOverMenuWindow = document.getElementById('popover-menu-window');
 const subMenuMobile = document.getElementById('submenu-mobile');
 
 resourcesButtonTablet.addEventListener('click', () => {
-    const node = document.createElement('div');
-    node.classList.add('menu-item-arrow');
     const currentPositionTop = resourcesButtonTablet.getBoundingClientRect().top;
     const currentPositionLeft = resourcesButtonTablet.getBoundingClientRect().left;
 
     popOverSubMenuList.style.top = `${(window.innerHeight - currentPositionTop) > 220
         ? currentPositionTop : (currentPositionTop - (220 - (window.innerHeight - currentPositionTop)))}px`;
-    const elements = document.getElementsByClassName("menu-item-arrow");
-    console.log(window.innerWidth);
     if (window.innerWidth > 720) {
         if (popOverSubMenu.style.marginLeft === '800px') {
             popOverSubMenu.style.marginLeft = `${currentPositionLeft - 220}px`;
-            resourcesButtonTablet.appendChild(node);
             popOverMenuWindow.style.boxShadow = 'none';
         } else {
             popOverSubMenu.style.marginLeft = '800px';
-            while (elements.length > 0) elements[0].remove();
             popOverMenuWindow.style.boxShadow = '-30px 0px 44px rgba(0, 0, 0, 0.4)';
         }
     } else {
         if (subMenuMobile.classList.contains('hide')) {
             subMenuMobile.classList.remove('hide');
-            node.style.transform = 'rotate(90deg)';
             resourcesButtonTablet.appendChild(node);
         } else {
             subMenuMobile.classList.add('hide');
-            while (elements.length > 0) elements[0].remove();
         }
     }
 });
